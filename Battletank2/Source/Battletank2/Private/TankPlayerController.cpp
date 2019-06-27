0b51872bbece17d7b5edae86cc3b66ca667ac68e
malith97 <reddogmalith@gmail.com>
2019-06-27 20:25:09 +0530

    creating an AI controller class

---
+++ Battletank2/Source/Battletank2/Private/TankPlayerController.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Battletank2.h"
+#include "TankPlayerController.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("player controller not possesing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("player controller is possesing a %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+
+{
+
+	return Cast<ATank>(GetPawn());
+}
+
+
